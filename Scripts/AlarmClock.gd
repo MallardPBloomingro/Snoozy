578d99618030c189681eba6f5624dc28e5cc792a
[JAM]Andreas <andreas.edmeier@web.de>
2018-09-01 15:15:39 +0200

    Menü gemacht und Speichern funktionsfähig gemacht

--- Scripts/AlarmClock.gd
+++ Scripts/AlarmClock.gd
@@ -12,6 +12,11 @@ func press():
 
 
 func _on_Timer_timeout():
+	#https://godotengine.org/qa/6491/read-&-write
+	var file = File.new()
+	if file.open("Cache.sav", File.WRITE) == 0:
+		file.store_line(nextScene)
+		file.close()
 	get_tree().change_scene(nextScene)
 
 
